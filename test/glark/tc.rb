40be0fe15ead496e9b830587ff83e3262af7521d
Jeff Pace <jeugenepace@gmail.com>
2012-11-04 15:54:02 -0500

    Removed unused test code.

--- test/glark/tc.rb
+++ test/glark/tc.rb
@@ -3,9 +3,7 @@
 
 require 'rubygems'
 require 'riel'
-
 require 'test/unit'
-require 'yaml'
 require 'tempfile'
 
 STDOUT.sync = true
@@ -31,52 +29,6 @@ class Glark::TestCase < Test::Unit::TestCase
     def instance_methods b
       unsorted_instance_methods(true).sort
     end
-  end
-  
-  def run_file_test fname, expected
-    info "fname: #{fname}".cyan
-
-    lnum = 0
-
-    File.open(fname) do |file|
-      file.each do |line|
-        line.chomp!
-        log "lnum    : #{lnum}"
-        log "expected: #{expected[lnum]}"
-        log "line    : #{line}"
-
-        assert_equal expected[lnum], line, "line #{lnum}"
-        lnum += 1
-      end
-    end
-    
-    assert_equal expected.length, lnum, "ending line number"
-    
-    File.delete fname
-  end
-
-  def create_file fname = nil
-    if fname
-      File.open(fname, File::CREAT | File::WRONLY | File::TRUNC) do |file|
-        yield file
-      end
-    else
-      tf = Tempfile.new "glark"
-      fname = tf.path
-      yield tf
-      tf.flush
-      tf.close
-    end
-    fname
-  end
-
-  def write_file contents, fname = nil
-    fname = create_file(fname) do |file|
-      for line in contents
-        file.puts line
-      end
-    end
-    fname
   end
 
   def test_truth
